33b86d0857e9f2f73765a1f03e22cb4ce2eb6a6d
Carlos Jorge Bernal Apaestegui <Carlos_Jorge32@hotmail.com>
2019-10-29 23:57:49 -0500

    nueva version

---
+++ Area de figuras geometricas/Area de figuras geometricas/Geometria.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Area_de_figuras_geometricas
+{
+    public partial class Geometria : Form
+    {
+        public Geometria()
+        {
+            InitializeComponent();
+        }
+    }
+}
