59e9915088d4c25e1e781d88cca038d96ee38927
Maickol <Maickol@DESKTOP-I5P97I4>
2019-10-29 22:31:24 -0500

    agregado area del octogono y romboide

---
+++ Area de figuras geometricas/Area de figuras geometricas/Area_del_romboide.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Area_de_figuras_geometricas
+{
+    public partial class Area_del_romboide : Form
+    {
+        public Area_del_romboide()
+        {
+            InitializeComponent();
+        }
+    }
+}
